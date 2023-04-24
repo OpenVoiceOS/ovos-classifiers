79165966c1b2e5220dc729b8e5822c57c7bc2248
JarbasAi <jarbasai@mailfence.com>
2023-04-24 06:21:57 +0100

    automations

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 def get_version():
     """ Find the version of the package"""
     version = None
-    version_file = os.path.join(BASEDIR, 'jurebes', 'version.py')
+    version_file = os.path.join(BASEDIR, 'ovos_classifiers', 'version.py')
     major, minor, build, alpha = (None, None, None, None)
     with open(version_file) as f:
         for line in f:
